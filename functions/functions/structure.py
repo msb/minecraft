eca9244f96edf15bb60b0908f1a0713210d3c556
Mike Bamford <mike@msb.me.uk>
2021-01-22 17:42:25 +0000

    Modfied the 'structure bedrock' tool to write 4 functions for each structure block rotated by 0, 90, 180, 270 degrees.

--- functions/functions/structure.py
+++ functions/functions/structure.py
@@ -1,7 +1,24 @@
 import os
+import numpy as np
 import bedrock
 from nbt.nbt import NBTFile, TAG_List, TAG_Compound
 from . import group_blocks_into_fills, write_fill
+
+# A list of 2D affine transform matrices for the rotations: 0, 90, 180, 270.
+ROTATION_MATRICES = (
+    np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1))),
+    np.array(((0, -1, 0), (1, 0, 0), (0, 0, 1))),
+    np.array(((-1, 0, 0), (0, -1, 0), (0, 0, 1))),
+    np.array(((0, 1, 0), (-1, 0, 0), (0, 0, 1))),
+)
+
+
+def rotate_90(voxel, origin=(0, 0, 0), times=0):
+    """
+    Rotates `voxel` about `origin` in the horizontal plane by 90 degrees `times` times.
+    """
+    rotated = ROTATION_MATRICES[times].dot((voxel[0] - origin[0], voxel[2] - origin[2], 0))
+    return (rotated[0] + origin[0], voxel[1], rotated[1] + origin[2])
 
 
 def unpack_nbt(tag):
@@ -77,7 +94,30 @@ def scan_volume(volume_lower, volume_upper):
                 yield (x, y, z)
 
 
-def get_block_name(strip_namespace, data_value_map, block):
+def add_data_values(rotation_group_map, data_value_map_for_block, data_value_name, data_values):
+    """
+    For a block's given `data_value_name`, this function adds it's data value to the current
+    `data_values`. If `data_value_name` has a rotation group, `data_values` is expanded to a list
+    of 4 data values and the rotation group values are added to each item.
+    """
+    if data_value_name in rotation_group_map:
+        rotation_group = rotation_group_map[data_value_name]
+        # find the match `data_value_name` in the `rotation_group` and start from there.
+        index = rotation_group.index(data_value_name)
+        return [
+            # it's safe to assume that `data_values` will be a list of one as there will only be
+            # one rotation group per block type.
+            data_values[0] + data_value_map_for_block[rotation_group[group_index % 4]]
+            for group_index in range(index, index + 4)
+        ]
+
+    return [
+        data_value + data_value_map_for_block[data_value_name]
+        for data_value in data_values
+    ]
+
+
+def get_block_name(strip_namespace, data_value_map, rotation_group_map, block):
     """
     A function that converts a block's name and properties into a name and data value that can be
     used in a bedrock fill function. Returning a null data value indicates that the block shouldn't
@@ -90,10 +130,16 @@ def get_block_name(strip_namespace, data_value_map, block):
 
     if len(block.properties) == 0:
         # if there aren't any propertied then just return the name.
-        return name
+        return [name]
 
     data_value_map_for_block = data_value_map.get(name, {})
-    data_value = 0
+
+    # Initially a list of a single data value. If the block has different data values for different
+    # rotations (eg. `facing_direction`) add_data_values() will change this into a list of four
+    # values, one for each direction.
+    data_values = [0]
+
+    # Add the appropriate data values for each block properties.
     for prop in block.properties:
         data_value_name = f'{prop.name}:{prop.payload}'
         if data_value_name not in data_value_map_for_block:
@@ -102,13 +148,15 @@ def get_block_name(strip_namespace, data_value_map, block):
             )
         if data_value_map_for_block[data_value_name] is None:
             # a null data value indicates that the block shouldn't be set (useful for doors)
-            data_value = None
+            data_values = None
             break
-        # data values for different properties represent different bits and can be added.
-        data_value += data_value_map_for_block[data_value_name]
-    if data_value is None:
+        data_values = add_data_values(
+            rotation_group_map, data_value_map_for_block, data_value_name, data_values
+        )
+
+    if data_values is None:
         return None
-    return f'{name} {data_value}'
+    return [f'{name} {data_value}' for data_value in data_values]
 
 
 def convert_bedrock(path_to_save, path_to_functions, settings):
@@ -121,6 +169,11 @@ def convert_bedrock(path_to_save, path_to_functions, settings):
         data_value_map = settings['data_value_map']
         strip_namespace = settings.get('strip_namespace')
 
+        # create a map of the rotation group, keyed of the groups constituent data value names.
+        rotation_group_map = {
+            item: group for group in settings.get('rotation_groups', []) for item in group
+        }
+
         def generate_volume(volume_lower, volume_upper):
             """
             A generator that yields all the blocks in a particular world's volume. The yielded
@@ -131,7 +184,9 @@ def convert_bedrock(path_to_save, path_to_functions, settings):
             for x, y, z in scan_volume(volume_lower, volume_upper):
                 block = world.getBlock(x, y, z)
                 if block is not None:
-                    name = get_block_name(strip_namespace, data_value_map, block)
+                    name = get_block_name(
+                        strip_namespace, data_value_map, rotation_group_map, block
+                    )
                     # a null can be returned, which means it should be ignored
                     if name is not None:
                         yield (x - lower_x, y - lower_y, z - lower_z, name)
@@ -178,7 +233,7 @@ def convert_bedrock(path_to_save, path_to_functions, settings):
             # order the fills, if an order has been defined
             if 'order_values' in settings:
                 order_values = settings['order_values']
-                fills.sort(key=lambda f: order_values.get(f[2], 50))
+                fills.sort(key=lambda f: order_values.get(f[2][0], 50))
 
             # the origin is set so that the structure will be created as if you were standing in
             # the same position as the structure block.
@@ -189,8 +244,19 @@ def convert_bedrock(path_to_save, path_to_functions, settings):
                 - structure_block['zStructureOffset'],
             )
 
-            # write out the function
-            function_file = os.path.join(path_to_functions, f'{structure_name}.mcfunction')
-            with open(function_file, 'w') as file:
-                for min_voxel, max_voxel, name in fills:
-                    write_fill(file, min_voxel, max_voxel, name, origin)
+            # write out a function for each 90 degree rotation.
+            for rotation in range(4):
+                function_file = os.path.join(
+                    path_to_functions, f'{structure_name}.{rotation}.mcfunction'
+                )
+                with open(function_file, 'w') as file:
+                    for min_voxel, max_voxel, name in fills:
+                        write_fill(
+                            file,
+                            rotate_90(min_voxel, origin, rotation),
+                            rotate_90(max_voxel, origin, rotation),
+                            # we use modulus because name can be an array of 1 if the block has no
+                            # rotations
+                            name[rotation % len(name)],
+                            origin
+                        )
